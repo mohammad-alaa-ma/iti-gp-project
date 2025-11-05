fd5420a20436f5c8c13ee41f635a0943aa78c0c7
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2025-11-05 21:20:54 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "test_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"   # Amazon Linux 2 AMI (us-east-1)
+  ami           = "ami-0ecb62995f68bb549"   # Amazon Linux 2 AMI (us-east-1)
   instance_type = "t2.micro"
 
   tags = {
