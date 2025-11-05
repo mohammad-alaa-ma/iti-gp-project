fb6f564c7e7a00d8a4183d1676ee0cfc424bc4e0
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2025-11-05 20:01:52 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"   # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-Test"
+  }
+}
